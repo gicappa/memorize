857a39004f33036e30bd7c623582bbb2f1588758
Gian Carlo Pace <giancarlo.pace@gmail.com>
2019-03-21 23:41:46 +0100

    Extracting method to ask for a quiz

--- src/main/java/memorize/App.java
+++ src/main/java/memorize/App.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 
 import static java.lang.System.exit;
 
+/**
+ * Entry point for the whole application.
+ * The journey starts here.
+ */
 public class App {
 
     private final ArrayList<Quiz> quizzes;
 
+    /**
+     * Main method where the application starts.
+     *
+     * @param args list of arguments.
+     */
     public static void main(String[] args) {
         try {
             var quizzes = new ArrayList<Quiz>();
+
             quizzes.add(new Quiz("Domanda?", "corretta"));
 
             new App(quizzes).run();
@@ -21,21 +31,38 @@ public class App {
         }
     }
 
+    /**
+     * Constructor of the main app
+     *
+     * @param quizzes list of quizzes as a parameter
+     */
     App(ArrayList<Quiz> quizzes) {
         this.quizzes = quizzes;
     }
 
+
+    /**
+     * starting all the things
+     */
     void run() {
         for (Quiz quiz : quizzes) {
-            displayQuestion(quiz.getDescription());
-            String answer = getUserAnswer();
+            askQuiz(quiz);
+        }
+    }
 
-            if (quiz.isCorrect(answer)) {
-                displaySuccess();
-            } else {
-                displayFailure();
-            }
+    /**
+     * Single quiz question
+     *
+     * @param quiz the quiz to be asked
+     */
+    void askQuiz(Quiz quiz) {
+        displayQuestion(quiz.getDescription());
+        String answer = getUserAnswer();
 
+        if (quiz.isCorrect(answer)) {
+            displaySuccess();
+        } else {
+            displayFailure();
         }
     }
 
